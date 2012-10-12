cb36c35b8fa9225a4daf4f848b8ec8835d14a4cd
Kristian Hentschel <me@kristianhentschel.com>
2012-10-12 10:15:31 +0100

    untested basic implementation of most mlist hash table stuff (no lookup, no resizing, noc hecking for duplicates yet)

--- ex1/mlist.c
+++ ex1/mlist.c
@@ -4,15 +4,49 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "mlist.h"
 
+#define INITIAL_SIZE 10
+#define MAX_BUCKET_SIZE 4
+
 int ml_verbose=0;		/* if true, print diagnostics on stderr */
+
+typedef struct mlistnode {
+	MEntry *entry;
+	struct mlistnode *next;
+} MListNode;
+
+struct mlist {
+	 struct mlistnode **buckets;
+	 int size;
+};
+
+static void mlistnode_destroy(MListNode *m);
 
 /* ml_create - created a new mailing list */
 MList *ml_create(void)
 {
-	/* TODO */
+	MList *p;
+	int i;
+
+	p = malloc(sizeof(struct mlist));
+	if(p == NULL)
+		return NULL;
+	
+	/* TODO provision for resizing, static implementation follows. */
+	
+	p->size = INITIAL_SIZE;
+	
+	p->buckets = malloc(p->size * sizeof(MListNode *));
+	for(i = 0; i < p->size; i++){
+		p->buckets[i] = NULL;
+	}
+
+	return p;
 }
+
+
 
 /* ml_add - adds a new MEntry to the list;
  * returns 1 if successful, 0 if error (malloc)
@@ -21,17 +55,58 @@ MList *ml_create(void)
  */
 int ml_add(MList **ml, MEntry *me)
 {
-	/* TODO */
+	unsigned long hash = me_hash(me, (*ml)->size);
+	MListNode *p, *tail;
+	
+
+	if(ml_verbose) fprintf(stderr, "ml_add: Entry with surname %s hashed to %d\n", me->surname, hash);
+
+	p = (*ml)->buckets[hash];
+	while( p != NULL ){
+		tail = p;
+		p = p->next;
+	}
+	p = malloc(sizeof(MListNode));
+	if (p == NULL)
+		return 0;
+
+	p->next = NULL;
+	p->entry = me;
+
+	tail->next = p;
+
+	return 1;
+
+	/* TODO check for duplicate and don't add. */
 }
+
 
 /* ml_lookup - looks for MEntry in the list, returns matching entry or NULL */
 MEntry *ml_lookup(MList *ml, MEntry *me)
 {
 	/* TODO */
+	return NULL;
 }
 
 /* ml_destroy - destroy the mailing list */
 void ml_destroy(MList *ml)
 {
 	/* TODO */
+	int i;
+
+	for (i = 0; i < ml->size; i++)
+		mlistnode_destroy(ml->buckets[i]);
+
+	free(ml);
+}
+
+/* frees entry, its value, and all following nodes in the linked list. */
+void mlistnode_destroy(MListNode *m)
+{
+	if (m == NULL)
+		return;
+
+	me_destroy(m->entry);
+	mlistnode_destroy(m->next);
+	free(m);
 }
