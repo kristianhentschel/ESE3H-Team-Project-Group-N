42ad6effc4538f242b82fc7e4678fb4704ca5c95
Kristian Hentschel <1003734h@student.gla.ac.uk>
2012-10-17 15:06:33 +0100

    not working findmain

--- AP3/lab3/findmain.c
+++ AP3/lab3/findmain.c
@@ -3,20 +3,41 @@
 
 #define MAXLINE 1000
 
-char pattern[] = "ould";
+void usage(char *bin) {
+	fprintf(stderr, "Usage: %s pattern\n", bin);
+}
 
 int main (int argc, char *argv[])
 {
 	char line[MAXLINE];
-
+	char *pattern;
 	int found = 0;
+	int invert = 0;
+
+	if (argc < 2 || argc > 3) {
+		usage(argv[0]);
+		return 1;
+	}
+	
+	if (argc > 2) {
+		if (*argv[1] == '-'){
+			if(strstr(argv[1], "x")){
+				invert = 1;
+			}
+		}
+	}
+
+	pattern = argv[i];
 
 	while (fgets(line, MAXLINE, stdin) != NULL) {
-		if (strstr(line, pattern) != NULL) {
+		if (strstr(line, pattern) != NULL ) {
+			printf("%s", line);
+			found++;
+		} else if ( invert ) {
 			printf("%s", line);
 			found++;
 		}
 	}
 
-	return found;
+	return 0;
 }
