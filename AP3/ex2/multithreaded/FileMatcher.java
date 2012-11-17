79a6c0d2043463ae8ce2b76cd5d7a0193c9e2180
Kristian Hentschel <kristian@kristianhentschel.com>
2012-11-17 22:49:33 +0000

    changed work queue to hold strings with an empty string as the terminator, rather than lugging an entire work-item construct around for all entries

--- AP3/ex2/multithreaded/FileMatcher.java
+++ AP3/ex2/multithreaded/FileMatcher.java
@@ -3,41 +3,37 @@ import java.io.*;
 import java.util.regex.*;
 
 public class FileMatcher implements Runnable {
-	private BlockingQueue<WorkItem> work_queue;
+	private BlockingQueue<String> work_queue;
 	private BlockingQueue<String> matches;
 	private final Pattern pattern;
 
-	public FileMatcher (BlockingQueue<WorkItem> work_queue, BlockingQueue<String> matches, Pattern pattern) {
+	public FileMatcher (BlockingQueue<String> work_queue, BlockingQueue<String> matches, Pattern pattern) {
 		this.work_queue = work_queue;
 		this.matches = matches;
 		this.pattern = pattern;
 	}
 
 	public void run() {
-		WorkItem item;
-
+		String dir;
 		try {
-			while( !(item = work_queue.take()).isKillswitch()) {
-				//Blocking Queue includes wait() and notifyAll()?
-				//System.err.printf("%s took:\t %s\n", Thread.currentThread().getName(), item.getName());
-				
-				File file = new File(item.getPath());	// create a File object
-				String entries[] = file.list();
-				// process directory child entries
+			//while the item taken from the queue is not an empty string:
+			while( !(dir = work_queue.take()).equals("")) {
+				File dirfd = new File(dir); // create a File object
+				String entries[] = dirfd.list();
+
 				for (String entry : entries ) {
 					//check if entry is itself a dir and skip if it is
-					File e = new File(item.getPath() + "/" + entry);
-					if (e.isDirectory())
+					File entryfd = new File(dir + "/" + entry);
+					if (entryfd.isDirectory())
 						continue;
 						
 					// apply regex pattern
 					Matcher m = pattern.matcher(entry);
 					if (m.matches()){
-						matches.put(item.getPath() + "/" + entry);
+						matches.put(dir + "/" + entry);
 					}
 				}
 			}
-			//System.err.printf("%s died\n", Thread.currentThread().getName());
 		} catch (InterruptedException e) {
 			System.err.printf("%s was interrupted\n", Thread.currentThread().getName());
 			return;
