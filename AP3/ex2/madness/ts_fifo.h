22d036063c6f953d73f8a2aa4c214164653f4be9
Kristian Hentschel <kristian@kristianhentschel.com>
2012-11-21 21:39:42 +0000

    moved type definitions around and simplified static function names for queue ADT

--- AP3/ex2/madness/ts_fifo.h
+++ AP3/ex2/madness/ts_fifo.h
@@ -2,26 +2,26 @@
 #define _TS_FIFO_H_
 
 /* public interface to thread safe fifo queue */
-typedef ts_fifo;
+typedef struct ts_fifo ts_fifo;
 
 /* creates and initialises an empty queue */
-ts_fifo ts_fifo_create();
+ts_fifo* ts_fifo_create();
 
 /* 
  * removes an item from the head of the queue.
  * blocks if the queue is empty, until an item becomes available.
  */
-void *ts_fifo_dequeue( ts_fifo q );
+void *ts_fifo_remove( ts_fifo *q );
 
 /*
  * adds an item to the tail of the queue
  * blocks until the lock is freed.
  */
-int ts_fifo_enqueue( ts_fifo q, void *item );
+int ts_fifo_add( ts_fifo *q, void *item );
 
 /*
  * destroys the queue and all nodes and items remaining in the queue.
  */
-void ts_fifo_destroy( ts_fifo q );
+void ts_fifo_destroy( ts_fifo *q );
 
 #endif /* _TS_FIFO_H_ */
