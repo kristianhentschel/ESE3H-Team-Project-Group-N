00be3c29b976f76adbb4e7682888201d4701fe62
Kristian <kristian@kristianhentschel.com>
2012-12-23 13:54:58 +0000

    experimented with http.server. seems to require too much custom code, will investigate alternatives to python 3 - maybe the 2.7 interface is better.

--- tp3/raspberrypi/server/src/helloworld.py
+++ tp3/raspberrypi/server/src/helloworld.py
@@ -1,5 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-print "Hello world server"
+#see also http://docs.python.org/3.3/library/http.server.html
+from http.server import * 
 
+
+
+
+
+class HelloHTTPRequestHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200, "Hello World")
+
+def run(server_class=HTTPServer, handler_class=HelloHTTPRequestHandler):
+	server_address = ('', 8000)
+	httpd = server_class(server_address, handler_class)
+	httpd.serve_forever()
+
+run();
 
