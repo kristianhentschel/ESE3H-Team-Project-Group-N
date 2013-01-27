3a400160955e82ba2f868e8fc90e70a9fb6664ab
Kristian Hentschel <kristian@kristianhentschel.com>
2013-01-27 16:45:24 +0000

    messed around in buffer reading...

--- ns3/ax1/server.c
+++ ns3/ax1/server.c
@@ -2,70 +2,83 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthreads.h>
+#include <pthread.h>
 #include <string.h>
-#include <types.h>
-
+#include <ctype.h>
+#include <errno.h>
 #define SERVER_PORT 8080
 #define SERVER_BACKLOG 16
+#define BUFFER_SIZE 64
 
 void handle_connection(int fd);
-void log(char *msg) {
-	sprintf(stderr, msg);
+void handle_request(int fd, char *request);
+
+void errlog(char *msg) {
+	fprintf(stderr, "%s\n", msg);
 }
 
 int main(void) {
 	int sockfd, connfd;
 	
 	sockfd = open_socket(SERVER_PORT);
+	bind(sockfd, SERVER_PORT);
 	listen(sockfd, SERVER_BACKLOG);
 	
-	while (true){
-		connfd = accept();
-		handle_connection(fd)
+	while (1){
+		connfd = accept(sockfd);
+		handle_connection(connfd);
 		close(connfd);
 	}
 
 	close(sockfd);
 }
 
+char *dynamic_strcat(char *s1, char *s2) {
+	char *result;
+   	if (!(result = malloc(strlen(s1) + strlen(s2) + 1))) {
+		errlog("malloc failed");
+		return NULL;
+	}
+	result[0] = '\0';
+	strcat(result, s1);
+	strcat(result, s2);
+	return result;
+}
 
 /* connection handler. reads from given socket descriptor, parses input stream, and sends responses. */
 void handle_connection(int fd){
-	char *dbuf, *oldbuf, *p;
-	char sbuf[BUFFER_SIZE];
-	int len, EOR_match;
-	const char EOR = "\r\n\r\n";	
-	
+	const char EOR[] = "\r\n\r\n";
+	unsigned int EOR_match;
+	int count, i, ri;
+	char buf[BUFFER_SIZE], req_buf[BUFFER_SIZE];
+	char *request, *temp;
+
 	EOR_match = 0;
-	while ((count = read(fd, &sbuf, BUFFER_SIZE - 1)) > 0) {
-		/* concatenate old buffer with new data */
-		len = len + count;
-		dbuf = malloc(len + count);
-		oldbuf = dbuf;
-		strcpy(dbuf, oldbuf);
-		p = dbuf + len;
-		memcpy(p, sbuf, count);
-		free(oldbuf);
-		
+	while ((count = read(fd, &buf, BUFFER_SIZE - 1)) > 0) {
 		/* check for end of request */
-		for(;p < len + count; p++) {
-			if(*p == EOR[EOR_match]) {
+		ri = 0;
+		for (i = 0; i < count; i++) {
+			if (buf[i] == EOR[EOR_match]) {
 				EOR_match++;
-				if (EOR_match == sizeof(EOR)) {
-					handle_request(dbuf);
-					oldbuf = dbuf;
-					dbuf = malloc(p - dbuf);
-					strcpy(dbuf, p);
-					free(oldbuf);
-
-
-					free(oldbuf)
+				if (EOR_match == strlen(EOR)) {
+					/* found end of request */
+					temp = request;
+					req_buf[ri] = '\0';
+					request = dynamic_strcat(request, req_buf);
+					
+					handle_request(fd, request);
+				
+					free(temp);
+					EOR_match = 0;
+					ri = 0;
+				}
 			} else {
 				EOR_match = 0;
+				req_buf[ri++] = buf[i];
 			}
+		}
 	}
 	if (count == -1) {
-		log(strerror("Read from socket failed"));
+		errlog(strerror(errno));
 	}
 }
