108d5de939c103df45a0c9d61873b635d842b132
Kristian Hentschel <1003734h@student.gla.ac.uk>
2013-02-07 15:48:20 +0000

    added some pseudocode to define structure of VM read.

--- os3/lab4/vm/VM.c
+++ os3/lab4/vm/VM.c
@@ -7,29 +7,28 @@
 #include "BackingStorePager.h"
 
 typedef unsigned long frame_t;
-typedef unsigned long page_t;
 typedef unsigned long process_t;
 typedef unsigned long vm_t;
 typedef unsigned long pm_t;
 
-struct pt_entry {
-	page_t	pagenumber;
-	frame_t	framenumber;
-	char dirty;
-};
-
-
+/* inverse page table maps from virtual base address and process id
+ * to page number in global page space. */
+struct ipt_entry {
+	process_t	process;
+	vm_t		virtual_base_address;
+	frame_t		frame_number;
+	char		dirty;
+}
 
 /* returns the physical memory address of the given virtual address in the given process's virtual address space.
  * will bring in the page from the backing store if required. */
-static unsigned long get_page(process_t pid, vm_t vaddr);
+static frame_t get_page(process_t pid, vm_t vaddr);
 
 /* selects a page and evicts it from physical memory, returns the now free frame number */
-static unsigned long evict_page();
+static frame_t evict_page();
 
 /* called once at start, used to initialise datastructures */
 void  initialise_VM(void) {
-
 }
 
 /* takes a ProcessId, a VM address and a datum */
@@ -40,4 +39,28 @@ void  VMWrite(unsigned long pid, unsigned long vaddr, unsigned long datum) {
 /* takes a ProcessId and a VM address, returns the datum */
 unsigned long  VMRead(unsigned long pid, unsigned long vaddr) {
 	return 0L;
+}
+
+
+
+static frame_t get_page(process_t pid, vm_t vaddr) {
+	/* find entry in inverted page table, using top bits of virtual address */
+
+	if (found) {
+		return frame number;
+	}
+
+		
+	/* get a free page */
+	if (!free_frames) {
+		frame = evict_page();
+	} else {
+		free_frames--;
+		frame = first_free_frame;
+	}
+	
+	/* bring the page in from the backing store */
+	BStoPM(free frame start address, pid, vaddr);
+	
+
 }
