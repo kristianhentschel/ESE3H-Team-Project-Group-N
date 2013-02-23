cdee99d410bf0c9c635221e9a0fbb25ee0a5a5f9
Kristian Hentschel <1003734h@student.gla.ac.uk>
2013-02-23 21:19:15 +0000

    moved definitions around, added global variables

--- os3/ax1/networkdriver.c
+++ os3/ax1/networkdriver.c
@@ -6,26 +6,31 @@
 #define APP_COUNT MAX_PID + 1
 #define TX_BUF_SIZE 10
 
+/* =====================================
+ * DATA TYPES & STATIC METHOD SIGNATURES 
+ */
+
 /* thread to await packets from the network and store them in the corresponding rx buffer. */
 void *thread_packet_receiver(void *arg);
 
 /* thread to take packets from the transmit queue and send them to the network device */
 void *thread_packet_transmitter(void *arg);
 
-/* receive buffer for a single application, holding a single packet. */
-typedef struct struct_simple_rx_buffer {
+/* receive buffer for a single application, holding a single packet.
+ * all data structures are bounded and use static memory. */
+typedef struct struct_simple_RXBuffer {
 	pthread_cond_t available;
 	PacketDescriptor packet;
-} rx_buffer;
+} RXBuffer;
 
 /* Overall container for receive buffers. hold one buffer for each pid. */
-typedef struct struct_rx_container{
+typedef struct struct_RXContainer{
 	pthread_mutex_t lock;
-	rx_buffer[APP_COUNT];
-} rx_container;
+	RXBuffer buffer[APP_COUNT];
+} RXContainer;
 
-/* Transmit buffer - implemented as a circular queue */
-typedef struct struct_tx_buffer {
+/* Transmit buffer - implemented as a static circular queue */
+typedef struct struct_TXBuffer {
 	pthread_mutex_t lock;
 	pthread_cond_t full;
 	pthread_cond_t empty;
@@ -33,11 +38,18 @@ typedef struct struct_tx_buffer {
 	int first;
 	int last;
 	PacketDescriptor packets[TX_BUF_SIZE];
-}
+} TXBuffer;
 
-/*
- * =================================
- * METHOD IMPLEMENTATIONS BEGIN HERE
+/* ================
+ * GLOBAL VARIABLES
+ */
+
+static FreePacketDescriptorStore global_FPDS;
+static TXBuffer global_TX;
+static RXContainer global_RX;
+
+/* ======================
+ * METHOD IMPLEMENTATIONS
  */
 
 void init_network_driver(NetworkDevice               nd, 
