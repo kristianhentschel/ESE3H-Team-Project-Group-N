7cd3c95d1496c5658a14e25626cdea6e1923c2ff
Kristian Hentschel <kristian@kristianhentschel.com>
2013-03-03 12:34:41 +0000

    added simple test implementation of master - no packet level parsing yet, just lexing and printing results.

---
+++ tp3/zb2/master_test.c
@@ -0,0 +1,37 @@
+#include "zb_transport.h"
+#include "zb_packets.h"
+
+/*
+ * master_test.c
+ *
+ * simple test application for sending, receiving, and parsing packets as the master unit.
+ * This will later be integrated with a web-server for a friendlier user interface.
+ *
+ * Author: Kristian Hentschel
+ * Team Project 3. University of Glasgow. 2013
+ */
+
+int main(void) {
+	char c;
+
+	zb_transport_open();
+
+	while(1){
+		c = zb_getc();
+		printf("%c", c);
+
+		switch (zb_parse(c)) {
+			case ZB_START_PACKET:
+				printf("\n(start of packet)\n");
+				break;
+			case ZB_PLAIN_WORD:
+				printf("\n(plain word of %d characters)\n", zb_word_len);
+				break;
+			case ZB_VALID_PACKET:
+				printf("\n(valid packet of %d characters)\n", zb_packet_len);
+				break;
+			default:
+				break;
+		}
+	}	
+}
