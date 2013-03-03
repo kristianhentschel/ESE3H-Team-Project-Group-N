e3ec6a2b04ec3b221ad5ba4e007680227a35d82e
Kristian Hentschel <kristian@kristianhentschel.com>
2013-03-03 12:14:22 +0000

    filled in implementation of getc and monitor thread

--- tp3/zb2/zb_transport_tty.c
+++ tp3/zb2/zb_transport_tty.c
@@ -24,22 +24,20 @@
 /* worker method. argument is a file descriptor for the serial interface */
 static void *serial_monitor(void *arg);
 
-
-
 typedef struct buffer {
 	pthread_mutex_t lock;
 	pthread_cond_t	nonfull;
 	pthread_cond_t	nonempty;
-	int size;
+	int count;
 	int last;
 	int first;
 	char elements[RECEIVE_BUFFER_SIZE];
 } Buffer;
 
-/* global variable to hold the receiver thread so it can be shut down later. */
+/* global variables */
 static pthread_t pthread_receiver;
-
-
+static Buffer RX_buffer;
+static int serial_fd;
 
 /* open and setup the serial device.
  * initialise the buffer structure, locks, and condition variables.
@@ -50,7 +48,12 @@ void zb_transport_init() {
 	int fd;
 
 	/* open serial port */
-	fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);
+	fd = open(SERIAL_DEVICE, O_RDWR | O_NOCTTY | O_NDELAY);
+	if (fd < 0) {
+		printf("[CRITICAL] could not open serial device %s\n", SERIAL_DEVICE);
+		exit(1);
+	}
+
 	fcntl(fd, F_SETFL, 0);
 	
 	/* set tc options for serial port transfers */
@@ -64,8 +67,79 @@ void zb_transport_init() {
 	tcsetattr(fd, TCSANOW, &tc);
 
 	/* set up buffer structures and locks */
-	/* TODO */
+	pthread_mutex_init(&RX_Buffer.lock, NULL);
+	pthread_mutex_lock(&RX_Buffer.lock);
+
+	pthread_cond_init(&RX_Buffer.nonfull, NULL);
+	pthread_cond_init(&RX_Buffer.nonempty, NULL);
+
+	RX_Buffer.count = RECEIVE_BUFFER_SIZE;
+	RX_Buffer.last = 0;
+	RX_Buffer.first = 0;
+
+	pthread_mutex_unlock(&RX_Buffer.lock);
 
 	/* start receiving thread to fill the buffer */	
 	pthread_create(&pthread_receiver, NULL, serial_monitor, (void *) fd);
+}
+
+/* close serial device, destroy any threads and locks (TODO do it properly) */
+void zb_transport_stop() {
+	pthread_cancel(pthread_receiver);
+	pthread_join(pthread_receiver);
+	close(serial_fd);
+}
+
+void zb_send(char *buf, char len) {
+	write(serial_fd, buf, len);
+}
+
+/* take a character from the buffer if it's not empty
+ * or block until a character is available.
+ */
+char zb_getc() {
+	char c;
+
+	pthread_mutex_lock(&RX_buffer.lock);
+	while (RX_buffer.count == 0) {
+		pthread_cond_wait(&RX_buffer.nonempty, &RX_buffer.lock);
+	}
+
+	c = RX_buffer.buffer[RX_buffer.first];
+	RX_buffer.count--;
+	RX_buffer.first = (RX_buffer.first + 1) % RX_BUFFER_SIZE;
+	
+	pthread_cond_signal(&RX_buffer.nonfull);
+	pthread_mutex_unlock(&RX_buffer.lock);
+
+	return c;
+}
+
+/* pause current thread for 1 second */
+void zb_guard_delay() {
+	/* TODO may want to use nanosleep() for POSIX conformity */
+	usleep(1000 * 1000);
+}
+
+/* worker thread for monitoring serial device and putting stuff into buffer */
+static void *serial_monitor(void *arg) {
+	char c;
+
+	while (read(serial_fd, &c, 1) > 0) {
+		pthread_mutex_lock(&RX_buffer.lock);
+		while (RX_buffer.count == RX_BUFFER_SIZE) {
+			pthread_cond_wait(&RX_buffer.nonfull, &RX_buffer.lock);
+		}
+		
+		RX_buffer.last = (RX_buffer.last + 1) % RX_BUFFER_SIZE;
+		
+		RX_buffer.buffer[RX_buffer.last] = c;
+		RX_buffer.count++;
+		
+		pthread_cond_signal(&RX_buffer.nonempty);
+		pthread_mutex_unlock(&RX_buffer.lock);
+	}
+
+	printf("[CRITICAL] read from serial device failed.\n");
+	return NULL;
 }
