8929bc801b625727a0496195ffcbd8edfcfe548e
Kristian Hentschel <kristian@kristianhentschel.com>
2013-03-04 09:19:49 +0000

    added skeleton comments to embedded transport level

---
+++ tp3/zb2/zb_transport_embedded.c
@@ -0,0 +1,34 @@
+#include <zb_transport.h>
+
+/*
+ * zb_transport_embedded.h
+ *
+ * Implementation of the serial receive buffer and send methods using interrupts
+ * and the STM32F4's USART peripherals.
+ *
+ */
+
+#define ZB_USART USART_4
+
+/* initialise USART Peripheral and set up GPIO pins for its use. Enable interrupts. */
+void zb_transport_init();
+
+/* disable interrupts for the usart peripheral */
+void zb_transport_stop();
+
+/* blocking write, sending character by character */
+void zb_send(char *buf, unsigned char len);
+
+/* disable interrupts, take character from buffer, re-enable interrupts */
+char zb_getc();
+
+/* delay for one second through SysTick or busy loop. */
+void zb_guard_delay();
+
+/* Interrupt handler for USART.
+ * 	- Check if interrupt source is RX buffer full
+ * 	- read from RX buffer
+ * 	- store in software RX buffer
+ * 	- clear interrupt flag
+ */
+void USART1_IRQHandler(void);
