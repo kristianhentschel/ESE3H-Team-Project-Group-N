327c37167e97163de1d3ae9bd06868bfa363eef1
Kristian Hentschel <khentschel@gmail.com>
2013-03-17 22:06:11 +0100

    made client web app flashier because I'm sick of writing the dissertation.

--- code/htdocs/script.js
+++ code/htdocs/script.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
-	var data_update_manually = false;
 	var data_update_timeout = 400;
+	var previous_measurement = null;
 
 	$(".screen").hide();
-	$("#screen-main").show();
+	$(".screen.main").show();
 	
 	$(".button.do-measure").click(function(){
-			$(".results li").removeClass("valid");
+			$(".results li").removeClass("invalid").removeClass("valid");
 			$.get("/api/measure/", function(data) {
+					//TODO handle "request ignored due to timeout" error response.
 					console.log(data);
-					if (!data_update_manually) {
-						setTimeout(data_update, data_update_timeout);
-					}
+					setTimeout(data_update, data_update_timeout);
 				});
 		});
 
 	$(".button.do-calibrate").click(function(){
-			$(".results li").removeClass("valid");
+			$(".results li").removeClass("invalid").removeClass("valid");
 			$.get("/api/calibrate/", function(data){
+					//TODO handle "request ignored due to timeout" error response.
+					setTimeout(data_update, data_update_timeout);
 					$(".screen").hide();
 					$(".screen.main").show();
 				});
 		});
-
 
 	$(".button.go-calibrate").click(function(){
 			$(".screen").hide();
@@ -40,26 +40,27 @@ $(document).ready(function(){
 		});
 
 
-
 	function data_update() {
 		$.getJSON("/api/data/", function(data){
 				console.log(data);
 				for (i = 1; i <= 4; i++) {
 					$("#data-" + i).text(data.sensors[i].converted);
 					$("#data-" + i + "-raw").text(data.sensors[i].value);
+					if (previous_measurement == null) {
+						if (data.sensors[i].time > 0) {
+							$("#data-" + i).closest("li").addClass("valid");
+						} else {
+							$("#data-" + i).closest("li").addClass("invalid");
+						}
+					} else if (data.sensors[i].time == previous_measurement.sensors[i].time) {
+						$("#data-" + i).closest("li").addClass("invalid");
+					} else {
+						$("#data-" + i).closest("li").addClass("valid");
+					}
 				}
-				$(".results li").addClass("valid");
+				previous_measurement = data;
 			});
 	
-	}
-
-
-	if (data_update_manually) {
-		$(".button.do-data").click(function(){
-			});
-	} else {
-		$(".do-data").hide();
-		data_update();
 	}
 });
 
